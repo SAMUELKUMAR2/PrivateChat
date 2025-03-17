165735676561fd443f11c92205f07aced42c35d5
SAMUELKUMAR2 <146331228+SAMUELKUMAR2@users.noreply.github.com>
2025-03-18 01:45:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(bodyParser.json()); // To parse JSON bodies
 
 // MongoDB connection
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));

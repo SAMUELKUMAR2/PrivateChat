68c26620871b4a8f11931c5fe4e1a7be1de132eb
SAMUELKUMAR2 <146331228+SAMUELKUMAR2@users.noreply.github.com>
2025-04-02 01:15:00 +0530

    Update LoginPage.jsx

--- private/src/LoginPage.jsx
+++ private/src/LoginPage.jsx
@@ -13,7 +13,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post(`localhost://5000/api/auth/login`, { name })
+    axios.post(`${API_URL}/api/auth/login`, { name })
       .then((response) => {
         if (response.data.role) {
           onLogin(response.data.role); // Pass role (admin or user) to the App component
@@ -28,10 +28,10 @@ const API_URL = import.meta.env.VITE_API_URL;
     <div style={{ height: '100vh' }} className='  w-100 d-flex flex-column justify-content-center align-items-center bg-primary '>
 
       <form className='d-flex gap-1' onSubmit={handleSubmit}>
-        <input className='rounded' type="text" value={name} onChange={handleInputChange} />
+        <input className='rounded px-2' type="text" value={name} onChange={handleInputChange} placeholder="write..."/>
         <button className='btn btn-success' type="submit">Submit</button>
       </form>
-     <div style="color:red;">{error && <p>{error}</p>}</div> 
+     <div style={{color:'red'}}>{error && <p>{error}</p>}</div> 
     </div>
   );
 }
